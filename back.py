f2a0bda1f2abb8633ba584bdb55acd19ac6d3c1d
Prantik Singh Chauhan <43614082+Dragon2298@users.noreply.github.com>
2019-10-24 02:15:13 +0530

    Add files via upload

--- back.py
+++ back.py
@@ -11,12 +11,12 @@ class Database:
 			)
 
     def insert_players(self,name,country,position,age,height,weight,rating):
-        self.cur.execute("INSERT INTO PLAYERS VALUES (%s,%s,%s,%s,%s,%s,%s)",(name,country,position,age,height,weight,rating))
+        self.cur.execute("INSERT INTO PLAYERS VALUES (%s,%s,%s,%s,%s,%s,%s)",(name,country,position,int(age),int(height),int(weight),int(rating)))
         self.conn.commit()
 
 	    
     def insert_teams(self,name,country,captain):
-        self.cur.execute("INSERT INTO TEAMS VALUES (NULL,%s,%s,%s,%s,%s,%s,%s)",(name,country,position,age,height,weight,rating))
+        self.cur.execute("INSERT INTO TEAMS VALUES (%s,%s,%s)",(name,country,captain))
         self.conn.commit()
 
 	    
@@ -26,12 +26,12 @@ class Database:
 
 	    
     def insert_goals(self,id,name,team,goals):
-        self.cur.execute("INSERT INTO GOALS_MATCH VALUES (%s,%s,%s,%s)",(id,name,team,int(goals)))
+        self.cur.execute("INSERT INTO GOALS_MATCH VALUES (%s,%s,%s,%s)",(int(id),name,team,int(goals)))
         self.conn.commit()
 
 	    
     def insert_match(self,id,t1,t2,loc,match_date):
-        self.cur.execute("INSERT INTO MATCHES VALUES (%s,%s,%s,%s,%s)",(id,t1,t2,loc,match_date))
+        self.cur.execute("INSERT INTO MATCHES VALUES (%s,%s,%s,%s,%s)",(int(id),t1,t2,loc,match_date))
         self.conn.commit()
 
 	    
@@ -62,17 +62,37 @@ class Database:
         rows=self.cur.fetchall()
         return rows    
 
-    def search(self,title="",author="",year="",isbn=""):
-        self.cur.execute("SELECT * FROM book WHERE title=? OR author=? OR year=? OR isbn=?", (title,author,year,isbn))
-        rows=self.cur.fetchall()
-        return rows
-
-    def delete(self,id):
-        self.cur.execute("DELETE FROM book WHERE id=?",(id,))
+    def delete_player(self,name):
+        self.cur.execute("DELETE FROM PLAYERS WHERE name=%s",(name,))
         self.conn.commit()
 
-    def update(self,id,title,author,year,isbn):
-        self.cur.execute("UPDATE book SET title=?, author=?, year=?, isbn=? WHERE id=?",(title,author,year,isbn,id))
+    def delete_team(self,name):
+        self.cur.execute("DELETE FROM TEAMS WHERE name=%s",(name,))
+        self.conn.commit()
+
+    def delete_match(self,id):
+        self.cur.execute("DELETE FROM MATCHES WHERE id=%s",(int(id),))
+        self.conn.commit()
+
+    def delete_booking(self,name):
+        self.cur.execute("DELETE FROM BOOKINGS WHERE name=%s",(name,))
+        self.conn.commit()
+
+
+    def update_player(self,name,country,position,age,height,weight,rating):
+        self.cur.execute("UPDATE PLAYERS SET country = %s, position = %s, age = %s, height = %s, weight = %s, rating = %s WHERE id=?",(country,position,int(age),int(height),int(weight),int(rating),name))
+        self.conn.commit()
+
+    def update_team(self,name,country,captain):
+        self.cur.execute("UPDATE TEAMS SET country=%s, captain=%s WHERE name = %s",(country,captain,name))
+        self.conn.commit()
+
+    def update_match(self,id,t1,t2,loc,match_date):
+        self.cur.execute("UPDATE MATCHES SET team1=%s, team2=%s, location=%s, match_date=%s WHERE id=%s",(t1,t2,loc,match_date,int(id)))
+        self.conn.commit()
+
+    def update_booking(self,name,yellow,red):
+        self.cur.execute("UPDATE book SET yellow_cards=%s, red_cards=%s WHERE name=%s",(int(yellow),int(red),name))
         self.conn.commit()
 
     def __del__(self):
