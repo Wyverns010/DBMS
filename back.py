c01649bd111340838b8ea3e6e845b5ab7850d2c2
Yash Bhardwaj <yash.bhardwaj5040@gmail.com>
2019-10-24 20:10:02 +0530

    updated cursor object

--- back.py
+++ back.py
@@ -9,38 +9,39 @@ class Database:
 			host = "127.0.0.1",
 			database = "fifa"
 			)
+        self.cur = self.conn.cursor();
 
     def insert_players(self,name,country,position,age,height,weight,rating):
         self.cur.execute("INSERT INTO PLAYERS VALUES (%s,%s,%s,%s,%s,%s,%s)",(name,country,position,int(age),int(height),int(weight),int(rating)))
         self.conn.commit()
 
-	    
+
     def insert_teams(self,name,country,captain):
         self.cur.execute("INSERT INTO TEAMS VALUES (%s,%s,%s)",(name,country,captain))
         self.conn.commit()
 
-	    
+
     def insert_bookings(self,name,yellow,red):
         self.cur.execute("INSERT INTO BOOKINGS VALUES (%s,%s,%s)",(name,int(yellow),int(red)))
         self.conn.commit()
 
-	    
+
     def insert_goals(self,id,name,team,goals):
         self.cur.execute("INSERT INTO GOALS_MATCH VALUES (%s,%s,%s,%s)",(int(id),name,team,int(goals)))
         self.conn.commit()
 
-	    
-    def insert_match(self,id,t1,t2,loc,match_date):
+
+    def insert_matches(self,id,t1,t2,loc,match_date):
         self.cur.execute("INSERT INTO MATCHES VALUES (%s,%s,%s,%s,%s)",(int(id),t1,t2,loc,match_date))
         self.conn.commit()
 
-	    
+
     def insert_playsfor(self,team,name):
         self.cur.execute("INSERT INTO PLAYS_FOR VALUES (%s,%s)",(team,name))
         self.conn.commit()
 
-	    
-	    
+
+
 
     def view_players(self):
         self.cur.execute("SELECT * FROM PLAYERS")
@@ -52,6 +53,11 @@ class Database:
         rows=self.cur.fetchall()
         return rows
 
+    def view_matches(self):
+        self.cur.execute("SELECT * FROM MATCHES")
+        rows=self.cur.fetchall()
+        return rows
+
     def view_scorers(self):
         self.cur.execute("SELECT * FROM GOALS")
         rows=self.cur.fetchall()
@@ -60,7 +66,7 @@ class Database:
     def view_bookings(self):
         self.cur.execute("SELECT * FROM BOOKINGS")
         rows=self.cur.fetchall()
-        return rows    
+        return rows
 
     def delete_player(self,name):
         self.cur.execute("DELETE FROM PLAYERS WHERE name=%s",(name,))
